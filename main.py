f4a8537bfeb088ccc5027651f4ef362466b538b2
terrasky064 <79109934+terrasky064@users.noreply.github.com>
2022-01-23 11:59:47 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-os.chdir("C:\Projects\Water_Quality_Project\Data")
+import plotly.express as px
+os.chdir(r"C:\Users\cyberkitsune096\Documents\Projects\water_quality_monitor-main\water_quality_monitor-main")
 csv_file='water_quality_data.csv'
 
 import csv
@@ -10,33 +11,56 @@ with open(csv_file, 'r') as file:
     for each_row in reader:
         print(each_row)
 
-
 data = pd.read_csv(csv_file)
-df = pd.DataFrame(data)
+
+usr_input = input("Input: ")
+while (usr_input != '1') and (usr_input != '2'):
+    usr_input = input("Input: ")
+
+if usr_input == '1':
+    fig1 = px.line(data, x = 'Date', y = 'pH', title='pH Date graph')
+    fig1.show()
+elif usr_input == '2':
+    fig2 = px.line(data, x = 'Date', y = 'Chlorine_Residual', title='Chlorine Residual Date graph')
+    fig2.show()
+
+
+df1 = pd.DataFrame(data)
 # Show the plot
 plt.show()
 
-'''date = data["Date"]
-time = data["Time"]
+#Unnamed 0 = date; Unnamed 3 = pH
+
+'''fig1 = px.line(data, x = 'Date', y = 'pH', title='pH Date graph')
+fig1.show()
+
+fig2 = px.line(data, x = 'Date', y = 'Chlorine_Residual', title='Chlorine Residual Date graph')
+fig2.show()'''
+
+'''#date = data['Date']
+#time = data["Time"]
 ph = data["pH"]
-chlorine = data["Chlorine"]
-residual = data["Residual"]
+chlorine = data["Chlorine Residual"]
 
 x = []
 y = []
 a = []
-b = []
 
-x=list(date, time)
-y=list(chlorine, residual)
+x=list(date)
+y=list(ph)
 a=list(chlorine)
-b=list(residual)
 
 plt.scatter(x,y)
-plt.xlabel('Date/Time->')
-plt.ylabel('Chlorine & Residual->')
+plt.xlabel('Date->')
+plt.ylabel('pH->')
 plt.title('Data')
-plt.show()'''
+plt.show()
+
+plt.scatter(x,y)
+plt.xlabel('Date->')
+plt.ylabel('Chlorine Residual->')
+plt.title('Data')
+plt.show()
 
 # Plot
 plt.figure(figsize=(6.8, 4.2))
@@ -45,4 +69,4 @@ plt.plot(x, data['pH'])
 plt.xticks(x, data['Date'])
 plt.xlabel('Date')
 plt.ylabel('pH')
-plt.show()
+plt.show()'''
